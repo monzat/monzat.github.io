f6c72c93cbf1b3ea0f62fa96f56a6775e42039e2
David Martins <davidmartins@Davids-MacBook-Pro.local>
2016-05-29 11:48:08 +0200

    test

--- es/js/form.js
+++
@@ -1,7 +0,0 @@
-  $(document).ready(function() {
-
-   $( "#service" ).change(function() {
-        $('#service').val()=="Other" ?  $('#service-other').show() : $('#service-other').hide(); $('#service-other input').val("");
-    });
-});
-
